98741b58de923e8331f8f7f383cf108761cc200c
hitesh-anand <ahitesh20@iitk.ac.in>
2022-11-03 19:35:11 +0530

    Added multiple sections

--- gulpfile.js
+++ gulpfile.js
@@ -11,13 +11,13 @@ const comments = require("gulp-header-comment");
 
 var deploy      = require('gulp-gh-pages');
 
-/**
- * Push build to gh-pages
- */
-gulp.task('deploy', function () {
-  return gulp.src("./dist/**/*")
-    .pipe(deploy())
-});
+// /**
+//  * Push build to gh-pages
+//  */
+// gulp.task('deploy', function () {
+//   return gulp.src("./dist/**/*")
+//     .pipe(deploy())
+// });
 
 var path = {
   src: {
